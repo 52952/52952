1e8ef87f03419220a457c1f6af03904640a25238
cmg <2652144732@qq.com>
2020-09-08 19:23:36 +0800

    Merge branch 'f1z'
    # Conflicts:
    #	src/cn/test.java

--- src/cn/test.java
+++ src/cn/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.printf("Hello world");
         System.out.println("f1z");
+        System.out.println("f8z");
     }
 }
